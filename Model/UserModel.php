56dc2963f14531eda93164b954433ddbbdbecafe
Maciej <mk@redingo.pl>
2017-01-08 22:08:57 +0100

    more structure

---
+++ Model/UserModel.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: maciej
+ * Date: 08/01/17
+ * Time: 22:05
+ */
+
+namespace Model;
+
+class UserModel
+{
+
+}
---
+++ Utilities/DatabaseFetcher.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: maciej
+ * Date: 08/01/17
+ * Time: 22:08
+ */
+
+namespace Utilities;
+
+
+class DatabaseFetcher
+{
+
+}
