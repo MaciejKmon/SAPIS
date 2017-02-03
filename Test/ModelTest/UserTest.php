90050071d76a0d51fb04326635c17b0fc80db105
Maciej <mk@redingo.pl>
2017-02-03 20:06:45 +0100

    Organising test methods

--- Test/ModelTest/UserTest.php
+++ Test/ModelTest/UserTest.php
@@ -11,24 +11,38 @@ class UserTest extends TestCase
      * @var User[]
      */
     private $users;
+    /**
+     * @var User
+     */
+    private $user1;
+    /**
+     * @var User
+     */
+    private $user2;
+
+    public function setUp(): void
+    {
+        $eoin = new User('Eoin', 'Eoin23password');
+        $natasha = new User('Nati', 'Natasha78password');
+        array_push($this->users, $eoin, $natasha);
+    }
+
+    public function testSetUsers(): void
+    {
+
+    }
 
     public function testGetUsers(): void
     {
         //For some reason I can't use this to get assertions
-        $this->createFixtures();
-
         $eoin = $this->users[0];
         TestCase::assertEquals(1, $eoin->getId());
         TestCase::assertEquals('Eoin',$eoin->getUsername());
         TestCase::assertEquals(md5('Eoin23password'), $eoin->getPassword());
-    }
 
-    private function createFixtures()
-    {
-        $users = [];
-        $eoin = new User('Eoin', 'Eoin23password');
-        $natasha = new User('Nati', 'Natasha78password');
-        array_push($users, $eoin, $natasha);
-        $this->users = $users;
+        $nati = $this->users[1];
+        TestCase::assertEquals(2, $nati->getId());
+        TestCase::assertEquals('Nati', $nati->getUsername());
+        TestCase::assertEquals(md5('Natasha78password'), $nati->getPassword());
     }
 }
