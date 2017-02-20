a2077843c1d151569e8a1b1273aa3cc174b7b48c
Maciej <mk@redingo.pl>
2017-02-20 23:26:58 +0100

    Fixes requested by Thunderer

--- Test/ModelTest/UserTest.php
+++ Test/ModelTest/UserTest.php
@@ -10,25 +10,17 @@ use Entity\User;
 
 class UserTest extends TestCase
 {
-    /**
-     * @var User[]
-     */
-    private $users;
-
-    public function setUp()
-    {
-        $this->users = [];
-        array_push($this->users, new User('Eoin', 'Eoin23password'), new User('Nati', 'Natasha78password'));
-    }
-
     public function testGetSet()
     {
-        $eoin = $this->users[0];
+        $users = [];
+        array_push($users, new User('Eoin', 'Eoin23password'), new User('Nati', 'Natasha78password'));
+
+        $eoin = $users[0];
         $eoin->setComments([$this->createMock(Comment::class), $this->createMock(Comment::class)]);
         $eoin->setProfile($this->createMock(Profile::class));
         $eoin->setStories([$this->createMock(Story::class)]);
 
-        $nati = $this->users[1];
+        $nati = $users[1];
         $nati->setComments([$this->createMock(Comment::class), $this->createMock(Comment::class)]);
         $nati->setProfile($this->createMock(Profile::class));
         $nati->setStories([$this->createMock(Story::class)]);
@@ -37,7 +29,7 @@ class UserTest extends TestCase
         $this->assertEquals(md5('Eoin23password'), $eoin->getPassword());
         $this->assertNotNull($eoin->getComments());
         $comments = $eoin->getComments();
-        $this->isInstanceOf(Comment::class, $comments[0]);
+        $this->assertInstanceOf(Comment::class, $comments[0]);
         $this->assertNotNull($eoin->getProfile());
         $this->assertInstanceOf(Profile::class, $eoin->getProfile());
         $this->assertNotNull($eoin->getStories());
