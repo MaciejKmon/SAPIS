0f4af2a60220f08d96807cb7cf74bd4b7001060c
Maciej <mk@redingo.pl>
2017-06-13 02:29:49 +0200

    [dev] Adjustment for security

--- src/AppBundle/Controller/AuthenticationController.php
+++ src/AppBundle/Controller/AuthenticationController.php
@@ -1,9 +1,32 @@
 <?php
-namespace src\AppBundle\Controller;
+namespace AppBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class AuthenticationController extends Controller
 {
-    
+    /**
+     * @Route("/login")
+     * @Method("POST")
+     */
+    public function loginAction (Request $request)
+    {
+        $data = $request->getContent();
+
+        $tokenAuth = $this->get('token.authenticator');
+
+        $user = $tokenAuth->;
+    }
+
+    /**
+     * @Route("/register")
+     * @Method("POST")
+     */
+    public function registerAction()
+    {
+
+    }
 }
