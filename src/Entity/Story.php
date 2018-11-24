df21e0536b29c9b0dc862074c4da42164b73ddaa
mkmon006 <pRqELo7x)rGY>
2018-11-24 22:45:43 +0100

    Update - moving SAPIS to Symfony 4.1

---
+++ src/Entity/Story.php
@@ -0,0 +1,135 @@
+<?php
+declare(strict_types=1);
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use AppBundle\Entity\Score;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="story")
+ */
+class Story
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="User")
+     * @ORM\JoinColumn(name="authorId", referencedColumnName="id")
+     */
+    private $author;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $body;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $title;
+
+    /**
+     * @ORM\OneToMany(targetEntity="Score", mappedBy="story")
+     */
+    private $scores;
+
+    /**
+     * @ORM\OneToMany(targetEntity="Comment", mappedBy="story")
+     */
+    private $comments;
+
+    public function __construct(User $author, string $body, string $title)
+    {
+        $this->author = $author;
+        $this->setBody($body);
+        $this->setTitle($title);
+        $this->setScores([]);
+        $this->setComments([]);
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBody(): string
+    {
+        return $this->body;
+    }
+
+    /**
+     * @param string $body
+     */
+    public function setBody(string $body)
+    {
+        $this->body = $body;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     */
+    public function setTitle(string $title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return User
+     */
+    public function getAuthor(): User
+    {
+        return $this->author;
+    }
+
+    /**
+     * @return Score[]
+     */
+    public function getScores(): array
+    {
+        return $this->scores;
+    }
+
+    /**
+     * @param Score $scores[]
+     */
+    public function setScores(array $scores)
+    {
+        $this->scores = $scores;
+    }
+
+    /**
+     * @return Comment[]
+     */
+    public function getComments(): array
+    {
+        return $this->comments;
+    }
+
+    /**
+     * @param Comment[] $comments
+     */
+    public function setComments(array $comments)
+    {
+        $this->comments = $comments;
+    }
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,178 @@
+<?php
+declare(strict_types=1);
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="user")
+ */
+class User implements UserInterface
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(name="username", type="string", length=100, unique=true)
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(name="password", type="string", length=255)
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $email;
+
+    /**
+     * @ORM\OneToOne(targetEntity="Profile", mappedBy="user", cascade={"persist"})
+     */
+    private $profile;
+
+    /**
+     * @ORM\OneToMany(targetEntity="Story", mappedBy="author")
+     */
+    private $stories;
+
+    /**
+     * @ORM\OneToMany(targetEntity="Comment", mappedBy="author")
+     */
+    private $comments;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $roles;
+
+    public function __construct(string $username, string $password, string $email)
+    {
+        $this->username = $username;
+        $this->setPassword($password);
+        $this->setEmail($email);
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string $password
+     */
+    public function setPassword(string $password)
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return Profile
+     */
+    public function getProfile(): Profile
+    {
+        return $this->profile;
+    }
+
+    /**
+     * @param Profile $profile
+     */
+    public function setProfile(Profile $profile)
+    {
+        $this->profile = $profile;
+    }
+
+    /**
+     * @return Story[]
+     */
+    public function getStories(): array
+    {
+        return $this->stories;
+    }
+
+    /**
+     * @param Story[] $stories
+     */
+    public function setStories(array $stories)
+    {
+        $this->stories = $stories;
+    }
+
+    /**
+     * @return Comment[]
+     */
+    public function getComments(): array
+    {
+        return $this->comments;
+    }
+
+    /**
+     * @param Comment[] $comments
+     */
+    public function setComments(array $comments)
+    {
+        $this->comments = $comments;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRoles()
+    {
+        return $this->roles;
+    }
+
+    /**
+     * @param string[]
+     */
+    public function setRoles(array $roles = ['ROLE_USER'])
+    {
+        $this->roles = $roles;
+    }
+
+    public function getSalt() {}
+
+    public function eraseCredentials() {}
+}
