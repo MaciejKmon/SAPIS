1b50b9e76e39bd2536e7d099bb94434af73b4d46
Maciej Kmon <kmon.maciej@gmail.com>
2020-03-22 20:39:54 +0100

    Post story form for validation

---
+++ src/Form/StoryType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Story;
+use Doctrine\DBAL\Types\TextType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class StoryType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('body')
+            ->add('title', TextType::class, [
+                'required' => true,
+            ])
+            ->add('author')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Story::class,
+        ]);
+    }
+}
